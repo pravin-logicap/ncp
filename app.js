f4df0c7f87f5152223f957ba572a14fa05b064b4
pravin-logicap <pravinlogicap@gmail.com>
2019-03-30 21:39:32 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'true');
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
 
