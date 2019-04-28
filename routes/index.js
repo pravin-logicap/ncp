4947aa6c40d8717d218233119d1e2e346d9a8fd3
Pravin <pravinlogicap@gmail.com>
2019-04-28 13:29:04 +0530

    Free html template added with PUG

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var dashboardService = require("../services/dashboardService");
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
 
 router.get("/dashboard", function (req, res) {
