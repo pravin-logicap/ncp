553a884b5c97e01cda6fe1a5ad6f2fa7fb61b7c9
Pravin <pravinlogicap@gmail.com>
2019-05-01 20:30:09 +0530

    Added real data from db

--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,28 @@ var router = express.Router();
 var dashboardService = require("../services/dashboardService");
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' });
+  dashboardService.getDashboardData()
+    .then(function (response) {
+      console.log("Sending response success");
+     // res.send(response);
+     //let dashboard = {"_id":"5c9f7641a36a7539919d47f7","noOfIndex":4,"breakingNews":{"title":"Supriya tai","imaageVideoUrl":"https://www.youtube.com/embed/tTNZn-facLM","shortDescription":"सुप्रिया सुळे यांनी लोकसभेत मांडले खासगी विधेयक, कर्मचाऱ्यांना मिळणार विशेष अधिकार","longDescription":"राष्ट्रवादी काँग्रेसच्या खासदा सुप्रिया सुळे (NCP MP Supriya Sule) यांनी एक खासगी विधेयक( Private Member's Bill) लोकसभेत दाखल केले आहे. राईट टू डिस्कनेक्ट (Right to Disconnect Bill) असे या विधेयकाचे नाव आहे. हे विधेयक लोकसभा सभागृहात संमत झाल्यास कार्यालयीन वेळ संपल्यानंतर कंपन्या, संस्था, अथवा कार्यालयांना कर्मचाऱ्यांकडून अधिकच्या कामाची तसेच वेळेची अपेक्षा ठेवता येणार नाही. कर्मचाऱ्यांना हा अधिकार मिळाल्यास कर्मचाऱ्यांवरअसलेला ताण कमी होईल तसेच, त्यांच्या कौटुंबिक, मानसिक व्यक्तिगत आयुष्यात मोठा बदल होईल, असे खा. सुळे यांचे ठाम विश्वास आहे. कार्यालयाची वेळ संपल्यानंतर कर्मचाऱ्याला कार्यालयीन जबाबदारीतुन मुक्त करण्यात यावे त्यासाठी कॉल डिस्कनेक्ट हा अधिकार कर्मचाऱ्याला मिळावा यासाठी त्यांनी हे विधेयक दाखल केले आहे.","dateAndTime":""},"indexes":[{"title":"Supriya tai Sule","imageVideoUrl":"https://img.etimg.com/thumb/height-480,width-640,imgsize-83822,msid-32778748/.jpg","newsReferenceNo":"1","totalNoOfNews":"3","indexRefernceNo":"1"},{"title":"Ajit dada Pawar","imageVideoUrl":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYhJkDKNqFRECdCTFceplUzPge_k57_zPQgO5uj4-zwhdP9NMW","newsReferenceNo":"1","totalNoOfNews":"10","indexRefernceNo":"2"},{"title":"Parth bhiya Pawar","imageVideoUrl":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRC4Yv49QNYKlbFRYZxWaFdqrQ0QMA7SwWN0mum7pNq7GhINUxa","newsReferenceNo":"1","totalNoOfNews":"1","indexRefernceNo":"3"},{"title":"Udyan Maharaj Bhosle","imageVideoUrl":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLZydyvIbgea5nBcnUSYA13lzHa-KX35ZF3RVbIIubVbSzStNh","newsReferenceNo":"1","totalNoOfNews":"3","indexRefernceNo":"4"}]};
+     let dashboard = response.response_data.result[0];
+     res.render('index', {dashboard});
+    })
+    .catch(function (error) {
+      console.log("Error occuered ");
+      res.send(error);
+    });
 });
 
 router.get("/dashboard", function (req, res) {
   dashboardService.getDashboardData()
     .then(function (response) {
       console.log("Sending response success");
-      res.send(response);
+     // res.send(response);
+     //let dashboard = {"_id":"5c9f7641a36a7539919d47f7","noOfIndex":4,"breakingNews":{"title":"Supriya tai","imaageVideoUrl":"https://www.youtube.com/embed/tTNZn-facLM","shortDescription":"सुप्रिया सुळे यांनी लोकसभेत मांडले खासगी विधेयक, कर्मचाऱ्यांना मिळणार विशेष अधिकार","longDescription":"राष्ट्रवादी काँग्रेसच्या खासदा सुप्रिया सुळे (NCP MP Supriya Sule) यांनी एक खासगी विधेयक( Private Member's Bill) लोकसभेत दाखल केले आहे. राईट टू डिस्कनेक्ट (Right to Disconnect Bill) असे या विधेयकाचे नाव आहे. हे विधेयक लोकसभा सभागृहात संमत झाल्यास कार्यालयीन वेळ संपल्यानंतर कंपन्या, संस्था, अथवा कार्यालयांना कर्मचाऱ्यांकडून अधिकच्या कामाची तसेच वेळेची अपेक्षा ठेवता येणार नाही. कर्मचाऱ्यांना हा अधिकार मिळाल्यास कर्मचाऱ्यांवरअसलेला ताण कमी होईल तसेच, त्यांच्या कौटुंबिक, मानसिक व्यक्तिगत आयुष्यात मोठा बदल होईल, असे खा. सुळे यांचे ठाम विश्वास आहे. कार्यालयाची वेळ संपल्यानंतर कर्मचाऱ्याला कार्यालयीन जबाबदारीतुन मुक्त करण्यात यावे त्यासाठी कॉल डिस्कनेक्ट हा अधिकार कर्मचाऱ्याला मिळावा यासाठी त्यांनी हे विधेयक दाखल केले आहे.","dateAndTime":""},"indexes":[{"title":"Supriya tai Sule","imageVideoUrl":"https://img.etimg.com/thumb/height-480,width-640,imgsize-83822,msid-32778748/.jpg","newsReferenceNo":"1","totalNoOfNews":"3","indexRefernceNo":"1"},{"title":"Ajit dada Pawar","imageVideoUrl":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYhJkDKNqFRECdCTFceplUzPge_k57_zPQgO5uj4-zwhdP9NMW","newsReferenceNo":"1","totalNoOfNews":"10","indexRefernceNo":"2"},{"title":"Parth bhiya Pawar","imageVideoUrl":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRC4Yv49QNYKlbFRYZxWaFdqrQ0QMA7SwWN0mum7pNq7GhINUxa","newsReferenceNo":"1","totalNoOfNews":"1","indexRefernceNo":"3"},{"title":"Udyan Maharaj Bhosle","imageVideoUrl":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLZydyvIbgea5nBcnUSYA13lzHa-KX35ZF3RVbIIubVbSzStNh","newsReferenceNo":"1","totalNoOfNews":"3","indexRefernceNo":"4"}]};
+     let dashboard = response.response_data.result[0];
+     res.render('index', {dashboard});
     })
     .catch(function (error) {
       console.log("Error occuered ");
